995886297831c03aa0a9282a8a2582e1c698001b
RStruijlaart <79460280+RStruijlaart@users.noreply.github.com>
2026-02-03 13:45:50 +0100

    added Movie and MovieScreening to domain

---
+++ Domain/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofaBioscoop.Domain;
+
+public class Movie
+{
+    private string title;
+
+    public Movie(string title)
+    {
+        this.title = title;
+    }
+
+    public void addScreening(MovieScreening movieScreening)
+    {
+        
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {title}";
+    }
+}
---
+++ Domain/MovieScreening.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofaBioscoop.Domain;
+
+public class MovieScreening
+{
+    private DateTime dateAndTime;
+    private double pricePerSeat;
+
+    public MovieScreening(Movie movie, DateTime dateAndTime, double pricePerSeat)
+    {
+        this.dateAndTime = dateAndTime;
+        this.pricePerSeat = pricePerSeat;
+        movie.addScreening(this);
+    }
+
+    public double getPricePerSeat()
+    {
+        return 1;
+    }
+
+    public override string ToString()
+    {
+        return $"Date and Time: {dateAndTime}, Price per seat: {pricePerSeat}";
+    }
+}
