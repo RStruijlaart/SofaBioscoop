57691166b99b6bc9e2af60a2aff3b5db82bff314
RSruijlaart <r@struijlaart.nl>
2026-02-06 19:30:48 +0100

    added test project

---
+++ SofaBioscoopTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+
+namespace SofaBioscoopTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int result = 1;
+            result.Should().Be(1);
+        }
+    }
+}
